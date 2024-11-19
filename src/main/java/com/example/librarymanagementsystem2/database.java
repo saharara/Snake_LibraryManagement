2689219c4dee993ce2f371afcbb93e9a778f7e63
nhhaanh2312 <your_username@users.noreply.github.com>
2024-11-19 16:58:58 +0700

    ok

--- src/main/java/com/example/librarymanagementsystem2/database.java
+++ src/main/java/com/example/librarymanagementsystem2/database.java
@@ -8,7 +8,7 @@ public class database {
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "nm515500214BM");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "23122004");
             return connect;
         } catch (Exception e) {
             e.printStackTrace();
