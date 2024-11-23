b8e820cf7bfdadba4de14f31310147161c4b5033
phamthucvietanh <23020514@vnu.edu.vn>
2024-11-23 20:17:18 +0700

    thay doi mk database

--- src/main/java/com/example/librarymanagementsystem2/database.java
+++ src/main/java/com/example/librarymanagementsystem2/database.java
@@ -7,7 +7,7 @@ public class database {
     public static Connection connectDB() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "23122004");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "nm515500214BM");
             return connect;
         } catch (Exception e) {
             e.printStackTrace();
