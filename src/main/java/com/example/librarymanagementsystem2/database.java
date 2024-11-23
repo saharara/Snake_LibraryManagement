58bd8a599c34cf5f661c29f514b631ef15c47256
phamthucvietanh <23020514@vnu.edu.vn>
2024-11-23 22:43:38 +0700

    test

--- src/main/java/com/example/librarymanagementsystem2/database.java
+++ src/main/java/com/example/librarymanagementsystem2/database.java
@@ -9,7 +9,7 @@ public class database {
 
             Class.forName("com.mysql.jdbc.Driver");
 
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "Hotchoco2005");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "nm515500214BM");
             return connect;
         } catch (Exception e) {
             e.printStackTrace();
